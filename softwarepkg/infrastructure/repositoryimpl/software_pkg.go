d8abc918c1e0ec82312ee25cf802db001b0f746b
zjm <1076115376@qq.com>
2023-02-28 20:18:06 +0800

    minor update

--- softwarepkg/infrastructure/repositoryimpl/software_pkg.go
+++ softwarepkg/infrastructure/repositoryimpl/software_pkg.go
@@ -3,21 +3,19 @@ package repositoryimpl
 import (
 	"errors"
 
-	"github.com/google/uuid"
 	"gorm.io/gorm"
 
 	repositoryerr "github.com/opensourceways/software-package-server/common/domain/repository"
 	"github.com/opensourceways/software-package-server/softwarepkg/domain"
 	"github.com/opensourceways/software-package-server/softwarepkg/domain/repository"
-	"github.com/opensourceways/software-package-server/utils"
-)
-
-const (
-	PackageName = "package_name"
 )
 
 type softwarePkgImpl struct {
 	db *gorm.DB
+}
+
+func NewSoftwarePkg(db *gorm.DB) repository.SoftwarePkg {
+	return softwarePkgImpl{db: db}
 }
 
 func (s softwarePkgImpl) SaveSoftwarePkg(pkg *domain.SoftwarePkgBasicInfo, version int) error {
@@ -46,50 +44,21 @@ func (s softwarePkgImpl) AddReviewComment(pid string, comment *domain.SoftwarePk
 }
 
 func (s softwarePkgImpl) AddSoftwarePkg(pkg *domain.SoftwarePkgBasicInfo) error {
-	if s.exists(pkg.Application.PackageName.PackageName()) {
-		return repositoryerr.NewErrorDuplicateCreating(errors.New("package name exists"))
-	}
-
 	softwareDO := s.toSoftwareDO(pkg)
 
 	return s.save(softwareDO)
 }
 
-func NewSoftwarePkg(db *gorm.DB) repository.SoftwarePkg {
-	return softwarePkgImpl{db: db}
-}
-
-func (s softwarePkgImpl) exists(packageName string) bool {
-	var soft SoftwarePkgDO
-	err := s.db.Model(soft).Where(PackageName+" = ?", packageName).First(&soft).Error
-	if err != nil {
-		return false
-	}
-
-	return true
-}
-
 func (s softwarePkgImpl) save(soft *SoftwarePkgDO) error {
-	return s.db.Model(soft).Create(soft).Error
-}
-
-func (s softwarePkgImpl) toSoftwareDO(pkg *domain.SoftwarePkgBasicInfo) *SoftwarePkgDO {
-	t := utils.Now()
-	softwareDO := &SoftwarePkgDO{
-		UUID:            uuid.New(),
-		SourceCode:      pkg.Application.SourceCode.Address.URL(),
-		PackageName:     pkg.Application.PackageName.PackageName(),
-		PackageDesc:     pkg.Application.PackageDesc.PackageDesc(),
-		PackageLicense:  pkg.Application.SourceCode.License.License(),
-		PackagePlatform: pkg.Application.PackagePlatform.PackagePlatform(),
-		PackageSig:      pkg.Application.ImportingPkgSig.ImportingPkgSig(),
-		PackageReason:   pkg.Application.ReasonToImportPkg.ReasonToImportPkg(),
-		Phase:           pkg.Phase.PackagePhase(),
-		// TODO pkg.Importer.Account() is nil
-		ImportUser: "",
-		CreateTime: t,
-		UpdateTime: t,
+	query := s.db.Where(&SoftwarePkgDO{PackageName: soft.PackageName}).FirstOrCreate(soft)
+	err := query.Error
+	if err != nil {
+		return err
 	}
 
-	return softwareDO
+	if query.RowsAffected == 0 {
+		return repositoryerr.NewErrorDuplicateCreating(errors.New("package name exists"))
+	}
+
+	return nil
 }
