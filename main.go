07cab3cac6e66a81f27042d45a0568f650022e69
zjm <1076115376@qq.com>
2023-03-07 15:57:52 +0800

    minor update

--- main.go
+++ main.go
@@ -77,7 +77,7 @@ func main() {
 	// Domain
 	dp.Init(&cfg.SoftwarePkg)
 
-	middleware.Init(cfg.Api.CheckUserUrl)
+	middleware.Init(cfg.Api.UserinfoUrl)
 
 	// run
 	server.StartWebServer(o.service.Port, o.service.GracePeriod, cfg)
